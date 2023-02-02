dfd608ebe3d76dea6eff5bbe218d934123d54021
Albérich <albe.ravier@hotmail.fr>
2023-02-02 15:58:32 +0100

    ajout ParcelleValue et fonction dans model Parcelle

--- src/main/java/eu/epfc/anc3/model/Parcelle.java
+++ src/main/java/eu/epfc/anc3/model/Parcelle.java
@@ -1,5 +1,25 @@
 package eu.epfc.anc3.model;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+
 public class Parcelle {
     //cellule
+    private final ObjectProperty<ParcelleValue> value = new SimpleObjectProperty<>(ParcelleValue.EMPTY);
+
+    ParcelleValue getValue() {
+        return value.getValue();
+    }
+    void setValue(ParcelleValue value) {
+        if(this.value.get() !=ParcelleValue.EMPTY)
+            this.value.setValue(value); //enleve grass
+        this.value.setValue(value); //met grass;.
+    }
+
+    ReadOnlyObjectProperty<ParcelleValue> valueProperty() {
+        return value;
+    }
+
+
 }
