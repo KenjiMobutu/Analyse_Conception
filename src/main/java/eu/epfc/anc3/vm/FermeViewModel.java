072db83fe05ab13f12e639ad0416fcae65063d88
KenjiMobutu <kenjimobutu@gmail.com>
2023-02-03 11:25:23 +0100

    feat(modelV FermeViewM) getters+ constructor added

--- src/main/java/eu/epfc/anc3/vm/FermeViewModel.java
+++ src/main/java/eu/epfc/anc3/vm/FermeViewModel.java
@@ -1,4 +1,34 @@
 package eu.epfc.anc3.vm;
 
+import eu.epfc.anc3.model.FermeFacade;
+import eu.epfc.anc3.view.TerrainView;
+import javafx.beans.Observable;
+import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.SimpleStringProperty;
+
 public class FermeViewModel {
+    private final MenuViewModel menuViewModel;
+    private final TerrainViewModel terrainViewModel;
+    private final FermeFacade ferme = new FermeFacade();
+
+    public MenuViewModel getMenuViewModel() {
+        return menuViewModel;
+    }
+    public ReadOnlyStringProperty titleProperty() {
+        return new SimpleStringProperty("Farm");
+    }
+
+    public ReadOnlyBooleanProperty isFermeStartedProperty() {
+        return ferme.isStartedProperty();
+    }
+
+    public TerrainViewModel getTerrainViewModel() {
+        return terrainViewModel;
+    }
+    public FermeViewModel(){
+        menuViewModel = new MenuViewModel(ferme);
+        terrainViewModel = new TerrainViewModel(ferme);
+
+    }
 }
