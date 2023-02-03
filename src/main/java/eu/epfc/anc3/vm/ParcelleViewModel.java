d1bbf34fbcf840c18d10f9ba22e27fa2b83acaf0
KenjiMobutu <kenjimobutu@gmail.com>
2023-02-03 11:27:03 +0100

    feat(VM ParcelleVM) constructor added

--- src/main/java/eu/epfc/anc3/vm/ParcelleViewModel.java
+++ src/main/java/eu/epfc/anc3/vm/ParcelleViewModel.java
@@ -1,5 +1,14 @@
 package eu.epfc.anc3.vm;
 
-public class ParcelleViewModel {
+import eu.epfc.anc3.model.FermeFacade;
 
+public class ParcelleViewModel {
+//cell
+    private final int line, col;
+    private FermeFacade ferme;
+    public ParcelleViewModel(int line, int col, FermeFacade ferme){
+        this.col = col;
+        this.line = line;
+        this.ferme = ferme;
+    }
 }
