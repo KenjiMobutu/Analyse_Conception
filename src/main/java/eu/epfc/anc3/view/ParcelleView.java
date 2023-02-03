7ad8c3cce8cb6d7e747aa5e1e46737d633da8945
KenjiMobutu <kenjimobutu@gmail.com>
2023-02-03 11:23:20 +0100

    feat(view ParcelleView) empty constructor added

--- src/main/java/eu/epfc/anc3/view/ParcelleView.java
+++ src/main/java/eu/epfc/anc3/view/ParcelleView.java
@@ -1,4 +1,14 @@
 package eu.epfc.anc3.view;
 
-public class ParcelleView {
+import eu.epfc.anc3.vm.ParcelleViewModel;
+import eu.epfc.anc3.vm.TerrainViewModel;
+import javafx.beans.binding.DoubleBinding;
+import javafx.scene.layout.StackPane;
+
+public class ParcelleView extends StackPane {
+    //cell
+    public ParcelleView(ParcelleViewModel parcelleViewModel, DoubleBinding parcelleWidthProperty){
+
+    }
+
 }
