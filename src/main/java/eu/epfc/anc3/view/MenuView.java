a1aa977d5b61df84ade6e90d1e9ab4f358d9eaee
KenjiMobutu <kenjimobutu@gmail.com>
2023-02-03 11:22:19 +0100

    feat(view MenuView) constructor + firsts methods added

--- src/main/java/eu/epfc/anc3/view/MenuView.java
+++ src/main/java/eu/epfc/anc3/view/MenuView.java
@@ -1,4 +1,40 @@
 package eu.epfc.anc3.view;
 
-public class MenuView {
+
+import eu.epfc.anc3.vm.MenuViewModel;
+import javafx.geometry.Insets;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
+
+import static eu.epfc.anc3.view.FermeView.MENU_WIDTH;
+import static eu.epfc.anc3.view.FermeView.PADDING;
+
+public class MenuView extends VBox {
+    // Composants du "menu"
+    private final Label nbHerbTxt = new Label("Nombre de parcelles de gazon : ");
+    private final Button startButton = new Button();
+    private final Button plantButton = new Button();
+    private final Button unPlantButton = new Button();
+
+    private final MenuViewModel menuViewModel;
+    public MenuView(MenuViewModel menuViewModel) {
+        this.menuViewModel = menuViewModel;
+        configMenu();
+    }
+
+    private void configMenu() {
+        setPadding(new Insets(PADDING));
+        setMinWidth(MENU_WIDTH);
+
+        startButton.setDisable(true);
+        configLabels();
+    }
+
+    private void configLabels() {
+        startButton.textProperty().bind(menuViewModel.startLabelProperty());
+        plantButton.textProperty().bind(menuViewModel.plantLabelProperty());
+        unPlantButton.textProperty().bind(menuViewModel.unPlantLabelProperty());
+    }
 }
