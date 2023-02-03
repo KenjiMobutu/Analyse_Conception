63ca765e620c0ff69a686f2f80d04040eb2ca064
KenjiMobutu <kenjimobutu@gmail.com>
2023-02-03 11:28:01 +0100

    feat(VM TerrainVM)method + constructor added

--- src/main/java/eu/epfc/anc3/vm/TerrainViewModel.java
+++ src/main/java/eu/epfc/anc3/vm/TerrainViewModel.java
@@ -1,4 +1,16 @@
 package eu.epfc.anc3.vm;
 
+import eu.epfc.anc3.model.FermeFacade;
+
 public class TerrainViewModel {
+    //grid
+    private final FermeFacade ferme;
+    public TerrainViewModel(FermeFacade ferme){
+        this.ferme = ferme;
+
+    }
+    public ParcelleViewModel getParcelleViewModel(int line, int col) {
+        return new ParcelleViewModel(line, col, ferme);
+    }
+
 }
