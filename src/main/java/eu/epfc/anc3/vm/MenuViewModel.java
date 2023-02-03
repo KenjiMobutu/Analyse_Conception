19c091ff6753fd572896935ededb90cdff56cd1d
KenjiMobutu <kenjimobutu@gmail.com>
2023-02-03 11:26:22 +0100

    feat(modelV MenuViewM) labels setters added

--- src/main/java/eu/epfc/anc3/vm/MenuViewModel.java
+++ src/main/java/eu/epfc/anc3/vm/MenuViewModel.java
@@ -1,4 +1,31 @@
 package eu.epfc.anc3.vm;
 
+import eu.epfc.anc3.model.FermeFacade;
+import javafx.beans.property.ReadOnlyStringProperty;
+import javafx.beans.property.SimpleStringProperty;
+import javafx.beans.value.ObservableValue;
+
 public class MenuViewModel {
+    private final FermeFacade ferme;
+
+    public MenuViewModel(FermeFacade ferme){
+        this.ferme = ferme;
+        configNameHandlers();
+        //configLogicStatus();
+    }
+
+    private void configNameHandlers() {
+
+    }
+
+    public ReadOnlyStringProperty startLabelProperty() {
+        return new SimpleStringProperty("Démarrer");
+    }
+
+    public ReadOnlyStringProperty plantLabelProperty() {
+        return new SimpleStringProperty("Planter du gazon");
+    }
+    public ReadOnlyStringProperty unPlantLabelProperty() {
+        return new SimpleStringProperty("Tondre du gazon");
+    }
 }
