c2cf4e09901067822e7376086cf5ebf78cf74fb1
KenjiMobutu <kenjimobutu@gmail.com>
2023-02-04 14:12:29 +0100

    feat(view FermeV) method configKeyPressed added

--- src/main/java/eu/epfc/anc3/view/FermeView.java
+++ src/main/java/eu/epfc/anc3/view/FermeView.java
@@ -1,6 +1,7 @@
 package eu.epfc.anc3.view;
 
 import eu.epfc.anc3.model.FermeFacade;
+import eu.epfc.anc3.model.Move;
 import eu.epfc.anc3.vm.FermeViewModel;
 import javafx.beans.binding.Bindings;
 import javafx.beans.property.DoubleProperty;
@@ -16,6 +17,7 @@ public class FermeView extends BorderPane {
     private static final int SCENE_MIN_WIDTH = 600;
     private static final int SCENE_MIN_HEIGHT = 400;
     static final int GRID_WIDTH = FermeFacade.gridWidth();
+    static final int FERME_WIDTH = 15;
 
     // Contrainte de mise en page
     private final DoubleProperty gridWidthProperty = new SimpleDoubleProperty(250);
@@ -36,9 +38,6 @@ public class FermeView extends BorderPane {
         // Mise en place de la scène et affichage de la fenêtre
         Scene scene = new Scene(this,SCENE_MIN_WIDTH,SCENE_MIN_HEIGHT);
 
-        //background music
-
-
         //configKeyPressed(scene);
         stage.setScene(scene);
         stage.show();
@@ -51,12 +50,10 @@ public class FermeView extends BorderPane {
 
         // Mise en place des composants du menu
         configMenu();
-
+        createTerrain();
         //Mise en place du Field du jeu
         configTerrainView();
 
-        //Mise en place du bouton start
-        //configPlayButton();
     }
 
     private void configTerrainView() {
@@ -79,9 +76,9 @@ public class FermeView extends BorderPane {
 
     private void createTerrain() {
         terrainView = new TerrainView(fermeViewModel.getTerrainViewModel(), gridWidthProperty);
+        System.out.println(terrainView);
         // Grille carrée
-        // Utilise des bindings (que nous verrons plus tard) : prenez ce code tel quel sans
-        // nécessairement le comprendre.
+        //terrainView.setStyle("-fx-background-color: #6e5b02;");
         terrainView.minHeightProperty().bind(gridWidthProperty);
         terrainView.minWidthProperty().bind(gridWidthProperty);
         terrainView.maxHeightProperty().bind(gridWidthProperty);
@@ -93,7 +90,29 @@ public class FermeView extends BorderPane {
 
     private void configMenu() {
         menuView = new MenuView(fermeViewModel.getMenuViewModel());
-        setBottom(menuView);
+        setTop(menuView.nbHerb);
+        setBottom(menuView.buttons);
+    }
+    public void configKeyPressed(Scene scene){
+        scene.setOnKeyPressed(keyEvent -> {
+            switch(keyEvent.getCode()) {
+                case RIGHT:
+                    fermeViewModel.keyPressed(Move.RIGHT);
+                    break;
+                case LEFT:
+                    fermeViewModel.keyPressed(Move.LEFT);
+                    break;
+                case UP:
+                    fermeViewModel.keyPressed(Move.UP);
+                    break;
+                case DOWN:
+                    fermeViewModel.keyPressed(Move.DOWN);
+                    break;
+                case SPACE:
+                    fermeViewModel.keyPressed(Move.SPACE);
+                    break;
+            }
+        });
     }
 
 }
