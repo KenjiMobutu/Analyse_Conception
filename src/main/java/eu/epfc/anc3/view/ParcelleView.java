c1c702893a0d2b3649cc61f1fe9db9f641b07e72
KenjiMobutu <kenjimobutu@gmail.com>
2023-02-04 14:15:34 +0100

    feat(view ParcelleV)constructor + setFermeImg added

--- src/main/java/eu/epfc/anc3/view/ParcelleView.java
+++ src/main/java/eu/epfc/anc3/view/ParcelleView.java
@@ -1,14 +1,49 @@
 package eu.epfc.anc3.view;
 
+import eu.epfc.anc3.model.Grass;
+import eu.epfc.anc3.model.ParcelleValue;
 import eu.epfc.anc3.vm.ParcelleViewModel;
 import eu.epfc.anc3.vm.TerrainViewModel;
 import javafx.beans.binding.DoubleBinding;
+import javafx.beans.property.ReadOnlyObjectProperty;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.StackPane;
 
 public class ParcelleView extends StackPane {
+    private static final Image FARMER = new Image("farmer.png");
+    private static final Image DIRT= new Image("dirt.png");
+    private static final Image GRASS = new Image("grass.png");
+    private final ImageView imageView = new ImageView();
     //cell
     public ParcelleView(ParcelleViewModel parcelleViewModel, DoubleBinding parcelleWidthProperty){
+        // imageView prend tout l'espace disponible et garde ses proportions
+        // Utilise des bindings (que nous verrons plus tard) : prenez ce code tel quel sans
+        // nécessairement le comprendre.
+        imageView.setPreserveRatio(true);
+        imageView.fitWidthProperty().bind(parcelleWidthProperty);
+        imageView.setImage(setFermeImg(parcelleViewModel.valueProperty().get()));
+        getChildren().add(imageView);
+        ReadOnlyObjectProperty<ParcelleValue> valueProp = parcelleViewModel.valueProperty();
+        valueProp.addListener((obs, old, newVal) -> imageView.setImage(setFermeImg(newVal)));
+
+        this.setOnMouseClicked(e -> parcelleViewModel.play());
 
     }
 
+    private void setFermeImage(ImageView imageView, ParcelleValue parcelleValue) {
+        imageView.setImage(parcelleValue == ParcelleValue.EMPTY
+                ? DIRT
+                : GRASS);
+    }
+    private Image setFermeImg(ParcelleValue parcelleValue) {
+        switch (parcelleValue){
+            case DIRT:
+                return DIRT;
+            case GRASS:
+                return GRASS;
+            default:
+                return null;
+        }
+    }
 }
