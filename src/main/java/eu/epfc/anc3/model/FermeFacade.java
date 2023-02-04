d84a4406b52b5c53ccae8d69f9d10cd6ad44feb7
KenjiMobutu <kenjimobutu@gmail.com>
2023-02-04 14:06:04 +0100

    feat(model FermeFacade) start+play,valueProperty+newGame method added

--- src/main/java/eu/epfc/anc3/model/FermeFacade.java
+++ src/main/java/eu/epfc/anc3/model/FermeFacade.java
@@ -3,6 +3,7 @@ package eu.epfc.anc3.model;
 import eu.epfc.anc3.view.FermeView;
 import javafx.beans.property.BooleanProperty;
 import javafx.beans.property.ReadOnlyBooleanProperty;
+import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.beans.property.SimpleBooleanProperty;
 
 public class FermeFacade {
@@ -22,7 +23,7 @@ public class FermeFacade {
 
 
     public FermeFacade(){
-        isStartable.bind(ferme.fermeStatusProperty().isEqualTo(FermeStatus.START));
+        //isStartable.bind(ferme.fermeStatusProperty().isEqualTo(FermeStatus.START));
 
         isStarted.bind(ferme.fermeStatusProperty().isNotEqualTo(FermeStatus.START));
 
@@ -39,4 +40,46 @@ public class FermeFacade {
     public ReadOnlyBooleanProperty isStartedProperty() {
         return isStarted;
     }
+
+    public void start() {
+        if (isStartable.get()) {
+            //ajouter validation si demandé ultérieurement
+            ferme.start();
+            farmerInFarm();
+        }
+    }
+
+    public ParcelleValue play(int line, int col) {
+        return ferme.play(line,col);
+    }
+
+    public ReadOnlyObjectProperty<ParcelleValue> valueProperty(int line, int col) {
+        return ferme.valueProperty(line, col);
+    }
+    public void farmerInFarm(){
+        ferme.farmerInFarm(farmer);
+    }
+    public void newGame() {
+        ferme.newGame();
+    }
+
+    /*public void moveFarmer(Move move) {
+        switch (move){
+            case UP:
+                goUp();
+                break;
+            case DOWN:
+                goDown();
+                break;
+            case LEFT:
+                goLeft();
+                break;
+            case RIGHT:
+                goRight();
+                break;
+            case SPACE:
+                dropGrass();
+                break;
+        }
+    }*/
 }
