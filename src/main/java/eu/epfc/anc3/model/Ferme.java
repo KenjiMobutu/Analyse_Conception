0dfc2efa9c676593f38e1e944cd7cd7fc0a6f933
KenjiMobutu <kenjimobutu@gmail.com>
2023-02-05 17:13:21 +0100

    refactor

--- src/main/java/eu/epfc/anc3/model/Ferme.java
+++ src/main/java/eu/epfc/anc3/model/Ferme.java
@@ -7,27 +7,37 @@ import javafx.beans.property.SimpleObjectProperty;
 public class Ferme {
 
 
-    private Terrain terrain = new Terrain();
+    private Terrain terrain= new Terrain();
     private Grass plantedGrass;
     private final ObjectProperty<FermeStatus> fermeStatus = new SimpleObjectProperty<>(FermeStatus.START);
 
     void start(){
-        newGame();
+        terrain = new Terrain();
+        fermeStatus.set(FermeStatus.STARTED);
+        //newGame();
     }
 
     private FermeStatus status(){return this.fermeStatus.get();}
 
-    ParcelleValue play(int x, int y){
+    ParcelleValue play(int line, int col){
         switch (status()){
             case START :
             case PLANT_GRASS: return ParcelleValue.GRASS;
             case DEPLANT_GRASS: return ParcelleValue.EMPTY;
+
         }
+
+
         /**
          * ajout de la fonction d'action du joueur (Terrain.play)
          */
         return ParcelleValue.EMPTY;
     }
+
+    /*private ParcelleValue getCurrentFarmerValue() {
+        //return STARTED;
+    }*/
+
     ReadOnlyObjectProperty<ParcelleValue> valueProperty(int line, int col) {
         return terrain.valueProperty(line, col);
     }
@@ -37,11 +47,12 @@ public class Ferme {
 
 
     public void newGame() {
-        fermeStatus.set(FermeStatus.START);
+        fermeStatus.set(FermeStatus.STARTED);
     }
 
     public void farmerInFarm(Farmer farmer) {
-        terrain.setValueOnField(farmer.getPosFarmer().getPosX(),farmer.getPosFarmer().getPosY(),ParcelleValue.FARMER);
+        System.out.println("Farmer");
+        terrain.setValueOnFarm(farmer.getPosFarmer().getPosX(),farmer.getPosFarmer().getPosY(),ParcelleValue.FARMER);
     }
     public Terrain getTerrain(){
         return terrain;
