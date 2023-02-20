ee31837eec523e893ae014cec8c1fa03a74c9603
KenjiMobutu <kenjimobutu@gmail.com>
2023-02-20 14:33:00 +0100

    feat(model Grass) Grass class

--- src/main/java/eu/epfc/anc3/model/Grass.java
+++ src/main/java/eu/epfc/anc3/model/Grass.java
@@ -1,4 +1,19 @@
 package eu.epfc.anc3.model;
 
 public class Grass {
+    private final Position position;
+
+    public Grass(Position position) {
+        this.position = position;
+    }
+
+    public Position getPosition() {
+        return position;
+    }
+
+    @Override
+    public String toString() {
+        return "Grass at " + "X:"+position.getPosX() +
+                "  Y:"+position.getPosY() ;
+    }
 }
