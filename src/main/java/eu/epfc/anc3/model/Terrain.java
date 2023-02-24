ee332d4ae8de8ca92e454f01ac037ee77652797d
alberich ravier <albe.ravier@hotmail.fr>
2023-02-24 17:15:54 +0100

    build (model Terrain) ré écriture des fichiers

--- src/main/java/eu/epfc/anc3/model/Terrain.java
+++ src/main/java/eu/epfc/anc3/model/Terrain.java
@@ -3,6 +3,10 @@ package eu.epfc.anc3.model;
 import javafx.beans.property.ReadOnlyObjectProperty;
 import javafx.scene.image.ImageView;
 
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
 public class Terrain {
     //grid
 
@@ -11,24 +15,58 @@ public class Terrain {
 
     private final Parcelle[][] matrix;
 
+    private final Set<Element>[][] elementsInCell;
+
     Terrain() {
         matrix = new Parcelle[GRID_HEIGHT][];
+        elementsInCell = new TreeSet[GRID_HEIGHT][];
         for (int i = 0; i < GRID_HEIGHT; ++i) {
             matrix[i] = new Parcelle[GRID_WIDTH];
+            elementsInCell[i] = new TreeSet[GRID_WIDTH];
             for (int j = 0; j < GRID_WIDTH; ++j) {
                 matrix[i][j] = new Parcelle();
+                elementsInCell[i][j] = new TreeSet<>();
             }
         }
     }
+
+    void resetTerrain() {
+        for (int i = 0; i < GRID_HEIGHT; ++i) {
+            for (int j = 0; j < GRID_WIDTH; ++j) {
+                matrix[i][j].setValue(ParcelleValue.DIRT);
+                elementsInCell[i][j].clear();
+            }
+        }
+    }
+
+
+
     ReadOnlyObjectProperty<ParcelleValue> valueProperty(int line, int col) {
         return matrix[line][col].valueProperty();
+    }
+
+
+    Set<Element> getAllElementsInCellFromSet(int line, int col) {
+        return elementsInCell[line][col];
+    }
+
+    void addElementToCell(Element e, int line, int col) {
+        if (!elementsInCell[line][col].contains(e))
+            elementsInCell[line][col].add(e);
+    }
+
+    void removeElemFromCell(int l, int c, Element e){
+        elementsInCell[l][c].remove(e);
+        System.out.println(elementsInCell[l][c]);
     }
 
     ParcelleValue getValue(int line, int col) {
         return matrix[line][col].getValue();
     }
-    boolean play(int line, int col, ParcelleValue playerValue) {
-      return matrix[line][col].setValue(playerValue);
+
+    //etait boolean avant
+    void play(int line, int col, ParcelleValue playerValue) {
+        matrix[line][col].setValue(playerValue);
     }
 
     private ParcelleValue checkCell(int line, int column){
@@ -48,7 +86,7 @@ public class Terrain {
     }
 
     public void setValueOnFarm(int x, int y, ParcelleValue value) {
-       matrix[x][y].setValue(value);
+        matrix[x][y].setValue(value);
     }
 
 
