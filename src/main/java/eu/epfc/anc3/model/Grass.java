adc4d99764d62daf4b9eeaa8a173a7540fadaedb
alberich ravier <albe.ravier@hotmail.fr>
2023-02-24 17:12:17 +0100

    build (model Grass) ré écriture du fichier en entier

--- src/main/java/eu/epfc/anc3/model/Grass.java
+++ src/main/java/eu/epfc/anc3/model/Grass.java
@@ -1,20 +1,29 @@
 package eu.epfc.anc3.model;
 
+import java.util.Objects;
+
 public class Grass {
+    private Position pos;
 
-    private final Position position;
-
-    public Grass(Position position) {
-        this.position = position;
-    }
-
-    public Position getPosition() {
-        return position;
+    public Grass(Position p){
+        this.pos = p;
     }
 
     @Override
     public String toString() {
-        return "Grass at " + "X:"+position.getPosX() +
-                "  Y:"+position.getPosY() ;
+        return this.pos.toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Grass grass = (Grass) o;
+        return Objects.equals(pos, grass.pos);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(pos);
     }
 }
