dc188f41d317203318c03a2a8439815ae5c94c75
alberich ravier <albe.ravier@hotmail.fr>
2023-02-24 17:14:50 +0100

    build (model Position/ParcelleValue) ré écriture des fichiers

--- src/main/java/eu/epfc/anc3/model/ParcelleValue.java
+++ src/main/java/eu/epfc/anc3/model/ParcelleValue.java
@@ -1,5 +1,5 @@
 package eu.epfc.anc3.model;
 
-public enum ParcelleValue {
-    GRASS, EMPTY,FARMER,DIRT
+public enum ParcelleValue implements Element{
+    GRASS,EMPTY,FARMER,DIRT
 }
--- src/main/java/eu/epfc/anc3/model/Position.java
+++ src/main/java/eu/epfc/anc3/model/Position.java
@@ -1,40 +1,38 @@
 package eu.epfc.anc3.model;
 
 public class Position {
+
     private int posX, posY;
+
     public Position(){
-        posY =0;
-        posX =0;
+
     }
 
     public Position(int x, int y){
-        posY = y;
-        posX = x;
+        this.posX =x;
+        this.posY =y;
     }
 
     public int getPosX() {
         return posX;
     }
 
+    public void setPosX(int posX) {
+        this.posX = posX;
+    }
+
     public int getPosY() {
         return posY;
     }
 
-    public void setPosX(int posX) {
-        this.posX = posX;
-    }
-    public void setPosY(int y) {
-        this.posY = y;
-    }
-
-
-    public void setPos(int posX,int posY) {
-        this.posX = posX;
+    public void setPosY(int posY) {
         this.posY = posY;
     }
-    public void getPos() {
-        getPosX();
-        getPosY();
-    }
 
+    public void getPos(){this.toString();}
+
+    @Override
+    public String toString() {
+        return  "X : " + getPosX() + " Y : " + getPosY();
+    }
 }
