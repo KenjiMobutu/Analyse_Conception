962dd0fd11c88db5564a15a72e3cf208d218278f
alberich ravier <albe.ravier@hotmail.fr>
2023-03-29 19:40:20 +0200

    modification comparator Element dans parcelle.java

--- src/main/java/eu/epfc/anc3/model/Parcelle.java
+++ src/main/java/eu/epfc/anc3/model/Parcelle.java
@@ -15,35 +15,42 @@ class Parcelle {
         @Override
         public int compare(Element e1, Element e2) {
             if (e1.getType() == ParcelleValue.FARMER) {
-                return 1; // farmer always first
-            } else if (e1.getType() == ParcelleValue.GRASS && e2.getType() != ParcelleValue.FARMER) {
-                return 1; // grass before other elements, except farmer
-            } else if (e2.getType() == ParcelleValue.FARMER || (e1.getType() == ParcelleValue.GRASS && e2.getType() == ParcelleValue.GRASS)) {
-                return -1; // farmer always first, or same element type
-            } else {
-                return 0; // other elements
-            }
-            /*if (e1.getType() == ParcelleValue.FARMER && e2.getType() == ParcelleValue.GRASS) {
-                return -1; // farmer always first
+                return 1; // farmer devant tous les éléments
             } else if (e2.getType() == ParcelleValue.FARMER) {
-                return 1; // farmer always first
+                return -1;
             } else if (e1.getType() == ParcelleValue.DIRT) {
-                return 1; // dirt always last
+                return 1; // la dirt devrait se trouver en dernier dans la liste
             } else if (e2.getType() == ParcelleValue.DIRT) {
-                return -1; // dirt always last
-            } else if (e1.getType() == ParcelleValue.GRASS) {
-                return -1; // grass before vegetables
-            } else if (e2.getType() == ParcelleValue.GRASS) {
-                return 1; // grass before vegetables
-            }*/ /*else if (e1.getType() == ParcelleValue.VEGETABLE) {
-                return 1; // vegetables cannot be stacked, last before dirt
-            } else if (e2.getType() == ParcelleValue.VEGETABLE) {
-                return -1; // vegetables cannot be stacked, last before dirt
-            }*/ //else {
-                //return 0; // same element type
+                return -1;
+            } else if (e1.getType() == ParcelleValue.GRASS) { // si dans la cellule il y a de l'herbe
+                if (e2.getType() == ParcelleValue.CARROT || e2.getType() == ParcelleValue.CABBAGE) {
+                    return -1; // je retourne les 'vegetables' après la grass
+                } else {
+                    return 1;
+                }
+            } else if (e2.getType() == ParcelleValue.GRASS) { // si on dépose de l'herbe après avoir déposer un 'vegetables' l'herbe doit se trouver derrière.
+                if (e1.getType() == ParcelleValue.CARROT || e1.getType() == ParcelleValue.CABBAGE) {
+                    return 1;
+                } else {
+                    return -1;
+                }
+            } else if (e1.getType() == ParcelleValue.CARROT || e1.getType() == ParcelleValue.CABBAGE) { // fais en sorte que les légumes > dirt/grass
+                if (e2.getType() == ParcelleValue.GRASS) {
+                    return -1;
+                } else {
+                    return 1;
+                }
+            } else {
+                return 0; // same element type
             }
-
+        }
     };
+
+
+
+
+
+
 
     private final ObservableSet<Element> elements = FXCollections.observableSet(
             new TreeSet<>(elementComparator)
@@ -66,10 +73,10 @@ class Parcelle {
     void addElement(Element e) {
         elements.add(e);
     }
-
-    void removeElement(Element e) {
-        elements.remove(e);
-    }
-
-    void clear(){elements.clear();}
+//
+//    void removeElement(Element e) {
+//        elements.remove(e);
+//    }
+//
+//    void clear(){elements.clear();}
 }
