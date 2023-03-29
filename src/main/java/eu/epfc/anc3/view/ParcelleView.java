93d27564b727e0ae8782e09c4f09c59b12977769
alberich ravier <albe.ravier@hotmail.fr>
2023-03-29 23:20:40 +0200

    remake ParcelleView utilisation d'une map a la place du foreach.

--- src/main/java/eu/epfc/anc3/view/ParcelleView.java
+++ src/main/java/eu/epfc/anc3/view/ParcelleView.java
@@ -13,6 +13,8 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.*;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class ParcelleView extends StackPane {
@@ -26,90 +28,42 @@ public class ParcelleView extends StackPane {
 
     private ImageView imageView = new ImageView();
 
+    private static final Map<ParcelleValue, Image> images = new HashMap<>();
+    static {
+        images.put(ParcelleValue.EMPTY, DIRT);
+        images.put(ParcelleValue.DIRT, DIRT);
+        images.put(ParcelleValue.GRASS, GRASS);
+        images.put(ParcelleValue.FARMER, FARMER);
+        images.put(ParcelleValue.CABBAGE, CABBAGE);
+        images.put(ParcelleValue.CARROT, CARROT);
+    }
 
 
     public ParcelleView(ParcelleViewModel parcelleViewModel, DoubleBinding parcelleWidthProperty) {
-        //imageView = new ImageView();
-
         imageView.setPreserveRatio(true);
         imageView.fitWidthProperty().bind(parcelleWidthProperty);
         imageView.fitHeightProperty().bind(parcelleWidthProperty);
         imageView.setImage(DIRT);
         getChildren().add(imageView);
 
-        //un listener sur l'ObservableSet<Element> et dedans parcourir ce truc en appelant getType() sur chaque element
         ObservableSet<Element> valueProp = parcelleViewModel.getElementsInCell();
 
         valueProp.addListener((SetChangeListener<Element>) change -> {
-            //vider tous les children
             ParcelleView.this.getChildren().clear();
             addParcelleImage(ParcelleValue.EMPTY);
-            for(Element e  : valueProp){
+            for(Element e : valueProp) {
                 addParcelleImage(e.getType());
-                //retrouver l'image de l'élement
-                //creer un nouvel imageView avec cette image
-                //ajouter cette imageView aux children
             }
-            //System.out.println(parcelleViewModel.line + " " + parcelleViewModel.col + " " +valueProp);
-            //System.out.println(parcelleViewModel.line + " " + parcelleViewModel.col + " " +ParcelleView.this.getChildren());
-            //pour chaque element de valueProp, creer une imageview et l'ajouter aux childrens'
-
         });
+
         this.setOnMouseClicked(e -> parcelleViewModel.play());
     }
 
-    void addParcelleImage(ParcelleValue pv){
-        switch (pv){
-            case EMPTY :
-            case DIRT:
-                addDirt();
-                break;
-            case GRASS:
-                addGrass();
-                break;
-            case FARMER:
-                addFarmer();
-                break;
-            case CABBAGE:
-                addCabbage();
-                break;
-            case CARROT:
-                addCarrot();
-                break;
-        }
+    void addParcelleImage(ParcelleValue pv) {
+        ImageView imageView = new ImageView(images.get(pv));
+        imageView.setFitWidth(this.imageView.getFitWidth());
+        imageView.setFitHeight(this.imageView.getFitHeight());
+        getChildren().add(imageView);
     }
 
-    private void addCarrot() {
-        ImageView carrot = new ImageView(CARROT);
-        carrot.setFitWidth(imageView.getFitWidth());
-        carrot.setFitHeight(imageView.getFitHeight());
-        getChildren().add(carrot);
-    }
-
-    private void addCabbage() {
-        ImageView cabbage = new ImageView(CABBAGE);
-        cabbage.setFitWidth(imageView.getFitWidth());
-        cabbage.setFitHeight(imageView.getFitHeight());
-        getChildren().add(cabbage);
-    }
-
-    void addDirt(){
-        ImageView dirt = new ImageView(DIRT);
-        dirt.setFitWidth(imageView.getFitWidth());
-        dirt.setFitHeight(imageView.getFitHeight());
-        getChildren().add(dirt);
-    }
-    void addGrass(){
-        ImageView grass = new ImageView(GRASS);
-        grass.setFitWidth(imageView.getFitWidth());
-        grass.setFitHeight(imageView.getFitHeight());
-        getChildren().add(grass);
-    }
-
-    void addFarmer(){
-        ImageView farmer = new ImageView(FARMER);
-        farmer.setFitWidth(imageView.getFitWidth());
-        farmer.setFitHeight(imageView.getFitHeight());
-        getChildren().add(farmer);
-    }
 }
