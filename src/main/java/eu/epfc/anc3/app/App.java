a82b1a6de28bef4fda1f50ffef31142bb100873e
KenjiMobutu <kenjimobutu@gmail.com>
2023-03-30 02:21:23 +0200

    feat(model)implementation for IT2  #3

--- src/main/java/eu/epfc/anc3/app/App.java
+++ src/main/java/eu/epfc/anc3/app/App.java
@@ -1,6 +1,7 @@
 //ANC3 2223 c01
 package eu.epfc.anc3.app;
 
+import eu.epfc.anc3.model.Carrot;
 import eu.epfc.anc3.view.FermeView;
 import javafx.application.Application;
 import javafx.scene.Scene;
@@ -22,6 +23,15 @@ public class App extends Application  {
     }
 
     public static void main(String[] args) {
-        launch();
+
+        //launch();
+        Carrot.CarrotState1 carrotState1 = new Carrot().new CarrotState1(new Carrot());
+        carrotState1.nextState();
+        Carrot.CarrotState2 carrotState2 = new Carrot().new CarrotState2(new Carrot());
+        carrotState2.nextState();
+        Carrot.CarrotState3 carrotState3 = new Carrot().new CarrotState3(new Carrot());
+        carrotState3.nextState();
+        Carrot.CarrotState4 carrotState4 = new Carrot().new CarrotState4(new Carrot());
+        carrotState4.nextState();
     }
 }
--- src/main/java/eu/epfc/anc3/model/ParcelleValue.java
+++ src/main/java/eu/epfc/anc3/model/ParcelleValue.java
@@ -2,5 +2,5 @@ package eu.epfc.anc3.model;
 
 public enum ParcelleValue{
 
-    EMPTY, DIRT,GRASS,FARMER,CABBAGE,CARROT
+    EMPTY, DIRT,GRASS,FARMER,CABBAGE, CARROT2, CARROT3, CARROT4, ROTTEN_CARROT, CARROT
 }
