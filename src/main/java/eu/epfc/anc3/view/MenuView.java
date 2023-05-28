5b9f2525e61529327daa402e81f274d6103c7e89
KenjiMobutu <kenjimobutu@gmail.com>
2023-05-29 00:22:44 +0200

    Exam : fonctionnel

--- src/main/java/eu/epfc/anc3/view/MenuView.java
+++ src/main/java/eu/epfc/anc3/view/MenuView.java
@@ -12,6 +12,8 @@ class MenuView extends VBox {
 
     private final Label scoreLabel = new Label("Score : ");
     private final TextField nbScore = new TextField("0");
+    private final Label graineLabel = new Label("Graine : ");
+    private final TextField nbGraine = new TextField("0");
     private final Label jourLabel = new Label("Jour : ");
     private final TextField nbJour = new TextField("0");
     private final Button startButton = new Button();
@@ -69,6 +71,7 @@ class MenuView extends VBox {
         // nbGrassTextField.setDisable(true);
 
         manageScore();
+        manageGraine();
         addToToggleGroup();
         bindLabelsToViewModel();
     }
@@ -87,6 +90,7 @@ class MenuView extends VBox {
         /*---------*/
         nbJour.setDisable(true);
         nbScore.setDisable(true);
+        nbGraine.setDisable(true);
         plantButtonGrass.setDisable(true);
         unPlantButton.setDisable(true);
         plantCabbageButton.setDisable(true);
@@ -134,6 +138,9 @@ class MenuView extends VBox {
     private void manageScore(){
         nbScore.textProperty().bind(menuViewModel.score().asString());
     }
+    private void manageGraine(){
+        nbGraine.textProperty().bind(menuViewModel.graine().asString());
+    }
 
 
     private void addToToggleGroup(){
@@ -157,7 +164,7 @@ class MenuView extends VBox {
     }
 
     HBox createNewHobx() {
-        return new HBox(scoreLabel,nbScore ,jourLabel,nbJour);//K:pour DEBUG
+        return new HBox(scoreLabel,nbScore ,jourLabel,nbJour, graineLabel, nbGraine);//K:pour DEBUG
     }
     public void setUpButtonStart() {
         startButton.setOnAction(e -> handleStartButtonAction());
